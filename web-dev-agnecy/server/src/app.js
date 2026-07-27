c8ce2ae50adfb9dd67ffaba5c15ed481b361bf68
arin-gupta06 <24cd10ar15@mitsgwl.ac.in>
2026-07-27 12:11:19 +0530

    Fix duplicate entry in allowedOrigins array

--- web-dev-agnecy/server/src/app.js
+++ web-dev-agnecy/server/src/app.js
@@ -9,7 +9,7 @@ const allowedOrigins = [
     process.env.CLIENT_URL,
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    
+    process.env.CLIENT_URL,
 ].filter(Boolean);
 
 app.use(cors({
